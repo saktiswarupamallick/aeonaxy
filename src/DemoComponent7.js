51b31e0cbd770695d8e7acfdf5d1f97f8be0a506
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-04-03 15:08:10 +0000

    #

--- src/DemoComponent7.js
+++ src/DemoComponent7.js
@@ -1,42 +1,48 @@
 import React from "react";
 
 const MainHeading = ({ children }) => {
-  return <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold">{children}</h1>;
+  return <h1 className="text-center mt-10 text-3xl md:text-4xl lg:text-5xl font-bold">{children}</h1>;
 };
 
 const SubHeading = ({ children }) => {
-  return <h2 className="text-center text-xl md:text-2xl lg:text-3xl font-semibold">{children}</h2>;
+  return <h2 className="text-center mt-8 text-md md:text-xl lg:text-x text-gray-400 ">{children}</h2>;
 };
 
 const Box = ({ image, heading, subheading }) => {
   return (
-    <div className="flex flex-row justify-center items-center bg-gray-200 p-4 border border-gray-300 rounded-lg">
-      <img src={image} alt="Box Image" className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 mb-4" />
+    <div className="flex mt-8 mx-6 flex-row-inverse justify-center items-center  p-4 border border-gray-300 rounded-lg">
       <div>
-        <h3 className="text-lg md:text-xl lg:text-2xl font-semibold">{heading}</h3>
-        <p className="text-sm md:text-base">{subheading}</p>
+        <h3 className="text-lg md:text-lg font-bold">{heading}</h3>
+        <div className="text-lg md:text-base">
+          {subheading.split('..').map((part, index) => (
+            <p key={index} className="mb-1">{part}</p>
+          ))}
+        </div>
       </div>
+      <div><img src={image} alt="Box Image" className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 mb-4" /></div>
+
+
     </div>
   );
 };
 
 const Page = () => {
   return (
-    <div className="container mx-auto py-8">
+    <div className="container mx-auto  py-8">
       <div>
-        <MainHeading>Main Heading</MainHeading>
-        <SubHeading>Sub Heading</SubHeading>
+        <MainHeading>Learning paths based on your answers</MainHeading>
+        <SubHeading>Choose one to get started. You can switch anytime.</SubHeading>
       </div>
       <div className="flex flex-col md:flex-row justify-center md:space-x-4 mt-8">
         <Box
           image="/parent.png"
           heading="Foundational Math"
-          subheading="Build your foundational skills in algebra ,geometry,and probability"
+          subheading="Build your foundational..skills in algebra ,geometry,..and probability."
         />
         <Box
           image="/parent.png"
           heading="Mathematical Thinking"
-          subheading="Build your foundational skills in algebra ,geometry,and probability"
+          subheading="Build your foundational..skills in algebra ,geometry,..and probability."
         />
       </div>
     </div>

129d3e3c0ef6cbd066c1357bd317cbecabf3cc2a
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-04-04 02:03:21 +0000

    #

--- src/DemoComponent3.js
+++ src/DemoComponent3.js
@@ -1,42 +1,47 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ProgressBar = ({ progress }) => {
-    return (
-      <div className="h-1 bg-green-500 ml-8 mr-8 mb-6 " style={{ width: `${progress}%` }}></div>
-    );
-  };
+  return (
+    <div className="h-1 bg-green-500 ml-8 mr-8 mb-6 " style={{ width: `${progress}%` }}></div>
+  );
+};
 
 const NextPage = () => {
-    const [progress, setProgress] = useState(0);
-    useEffect(() => {
-        
-        setProgress(60);
-      }, []);
-    
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+
+    setProgress(60);
+  }, []);
+
+  const handleContinue = () => {
+
+    navigate("/DemoComponent2");
+  };
+
   return (
     <>
-    <div className=" mx-auto mt-2 p-4">
-      <ProgressBar progress={progress} />
-    </div>
-    <div className="flex flex-col justify-center items-center h-screen overflow-auto">
-      {/* Container for Left and Right Sections */}
-      <div className="flex flex-col lg:flex-row justify-center items-center w-full max-w-screen-lg">
-        {/* Left Section */}
-        <div className="w-full lg:w-1/2 mb-8 lg:mb-0 pr-8"> {/* Added pr-4 for right padding */}
-          <img src="/rightpath.jpg" alt="Your Image" className="w-full" style={{ maxWidth: '700px' }} />
+      <div className=" mx-auto mt-2 p-4">
+        <ProgressBar progress={progress} />
+      </div>
+      <div className="flex flex-col justify-center items-center h-screen overflow-auto">
+        {/* Container for Left and Right Sections */}
+        <div className="flex flex-col lg:flex-row justify-center items-center w-full max-w-screen-lg">
+          {/* Left Section */}
+          <div className="w-full lg:w-1/2 mb-8 lg:mb-0 pr-8"> {/* Added pr-4 for right padding */}
+            <img src="/rightpath.jpg" alt="Your Image" className="w-full" style={{ maxWidth: '700px' }} />
+          </div>
+          {/* Right Section */}
+          <div className="w-full lg:w-1/2 px-8">
+            <h1 className="text-3xl lg:text-4xl font-bold mb-4">You're in the right place</h1>
+            <p className="text-base lg:text-lg text-gray-600 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat.</p>
+          </div>
         </div>
-        {/* Right Section */}
-        <div className="w-full lg:w-1/2 px-8">
-          <h1 className="text-3xl lg:text-4xl font-bold mb-4">You're in the right place</h1>
-          <p className="text-base lg:text-lg text-gray-600 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat.</p>
+        {/* Button */}
+        <div className="flex justify-center">
+          <button className="py-3 px-6 bg-gray-800 text-white rounded mt-8">Continue</button>
         </div>
       </div>
-      {/* Button */}
-      <div className="flex justify-center">
-        <button className="py-3 px-6 bg-gray-800 text-white rounded mt-8">Continue</button>
-      </div>
-    </div>
     </>
   );
 };

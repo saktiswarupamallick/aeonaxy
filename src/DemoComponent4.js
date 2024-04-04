685591f2a41975868d9248ca2cc2dcfd44dab68e
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-04-04 16:31:35 +0000

    #

--- src/DemoComponent4.js
+++ src/DemoComponent4.js
@@ -20,31 +20,31 @@ function BoxGrid() {
                     <p className="text-center mt-2 text-gray-600 dark:text-gray-300">Choose the highest level you feel confident in -you can always adjust later</p>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-6">
                         <div className="bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900 rounded-xl overflow-hidden" onClick={handleClick}>
-                            <img src="image1.jpg" alt="1" className="w-full h-40 object-cover" />
+                            <img src="/arithmetic.png" alt="1" className="w-full h-40 object-cover" />
                             <div className="p-6">
-                                <h2 className="text-xl text-gray-900 dark:text-white font-semibold">Arithmetic</h2>
-                                <p className="mt-2 text-gray-600 dark:text-gray-300">Introductory</p>
+                                <h2 className="text-md text-center text-gray-900 dark:text-white font-semibold">Arithmetic</h2>
+                                <p className="mt-2 text-gray-600 text-center text-lg dark:text-gray-300">Introductory</p>
                             </div>
                         </div>
                         <div className="bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900 rounded-xl overflow-hidden" onClick={handleClick}>
-                            <img src="image2.jpg" alt="2" className="w-full h-40 object-cover" />
+                            <img src="/algebra.png" alt="2" className="w-full h-40 object-cover" />
                             <div className="p-6">
-                                <h2 className="text-xl text-gray-900 dark:text-white font-semibold">Basic Algebra</h2>
-                                <p className="mt-2 text-gray-600 dark:text-gray-300">Foundational</p>
+                                <h2 className="text-md text-center  text-gray-900 dark:text-white font-semibold">Basic Algebra</h2>
+                                <p className="mt-2 text-gray-600 text-center text-lg dark:text-gray-300">Foundational</p>
                             </div>
                         </div>
                         <div className="bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900 rounded-xl overflow-hidden" onClick={handleClick}>
-                            <img src="image3.jpg" alt="3" className="w-full h-40 object-cover" />
+                            <img src="/quadratic.png" alt="3" className="w-full h-40 object-cover" />
                             <div className="p-6">
-                                <h2 className="text-xl text-gray-900 dark:text-white font-semibold">Intermediate Algebra</h2>
-                                <p className="mt-2 text-gray-600 dark:text-gray-300">Intermediate</p>
+                                <h2 className="text-md text-center text-gray-900 dark:text-white font-semibold">Intermediate Algebra</h2>
+                                <p className="mt-2 text-gray-600 text-center text-lg dark:text-gray-300">Intermediate</p>
                             </div>
                         </div>
                         <div className="bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900 rounded-xl overflow-hidden" onClick={handleClick}>
-                            <img src="image4.jpg" alt="4" className="w-full h-40 object-cover" />
+                            <img src="/calculus.png" alt="4" className="w-full h-40 object-cover" />
                             <div className="p-6">
-                                <h2 className="text-xl text-gray-900 dark:text-white font-semibold">Heading 4</h2>
-                                <p className="mt-2 text-gray-600 dark:text-gray-300">Subheading 4</p>
+                                <h2 className="text-md text-center text-gray-900 dark:text-white text-center font-semibold">calculus</h2>
+                                <p className="mt-2 text-gray-600 text-center text-lg dark:text-gray-300">Advanced</p>
                             </div>
                         </div>
                     </div>

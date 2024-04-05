f3f26d411278a8f057015c7742d263a65d76c00b
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-04-05 12:20:51 +0000

    #

--- src/DemoComponent5.js
+++ src/DemoComponent5.js
@@ -8,30 +8,24 @@ const ProgressBar = ({ progress }) => {
 };
 
 const DemoComponent5 = () => {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleContinue = () => {
-   
     navigate("/spinner");
   };
 
-
   return (
     <>
-    <div className=" mx-auto mt-2 p-4">
-      <ProgressBar progress={70} />
-    </div>
-    <div className="flex flex-col justify-center items-center h-screen overflow-auto">
-      {/* Container for Left and Right Sections */}
-      <div className="flex flex-col lg:flex-row justify-center items-center w-full max-w-screen-lg">
-        {/* Left Section */}
-        <div className="w-full lg:w-1/2 mb-8 lg:mb-0 pr-8"> 
-          <img src="/cheerful.jpg" alt="Your " className="w-full" style={{ maxWidth: '700px' }} />
-        </div>
-        {/* Right Section */}
-        <div className="w-full lg:w-1/2 px-8">
+      <div className="mx-auto mt-2 p-4 ">
+        <ProgressBar progress={90} />
+      </div>
+      <div className="flex flex-col justify-center items-center h-screen overflow-auto">
+        <div className="flex flex-col lg:flex-row justify-center items-center w-full max-w-screen-lg">
+          <div className="w-full lg:w-1/2 mb-8 lg:mb-0 pr-8">
+            <img src="/cheerful.jpg" alt="Your" className="w-full" style={{ maxWidth: '700px' }} />
+          </div>
+          <div className="w-full lg:w-1/2 px-8">
             <h1 className="text-3xl lg:text-4xl font-bold mb-4">You're on your way</h1>
-            {/* 5 Star SVG */}
             <div className="flex mt-10 justify-start">
               {[...Array(5)].map((_, index) => (
                 <svg key={index} className="w-7 h-8 text-yellow-500 mb-2 mr-1" fill="currentColor" viewBox="0 0 20 20">
@@ -39,18 +33,15 @@ const DemoComponent5 = () => {
                 </svg>
               ))}
             </div>
-            {/* Quote */}
-            <p className="italic text-center text-sm md:text-base mb-4">"Mathematics is not just about numbers and equations; it's about understanding patterns, uncovering hidden truths, and exploring the beauty of logic in the universe."</p>
-            {/* Author */}
+            <p className="italic  text-sm md:text-base mb-4">"Mathematics is not just about numbers and equations; it's about understanding patterns, uncovering hidden truths, and exploring the beauty of logic in the universe."</p>
             <p className="italic">- Jacob S.</p>
           </div>
         </div>
-      {/* Button */}
-      <div className="flex justify-center">
-        <button  onClick={handleContinue} className="py-3 px-6 bg-gray-800 text-white rounded mt-8">Continue</button>
+        <div className="flex justify-center">
+          <button onClick={handleContinue} className="py-3 px-6 bg-gray-800 text-white rounded mt-8">Continue</button>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
